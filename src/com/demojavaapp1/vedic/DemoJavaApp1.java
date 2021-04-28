cb5103b140d3882dbcabef7f9b8ba5b8f53f82e5
Latif Vedic <latif@vedic.com>
2021-04-28 16:05:44 +0530

    2st commit

--- src/com/demojavaapp1/vedic/DemoJavaApp1.java
+++ src/com/demojavaapp1/vedic/DemoJavaApp1.java
@@ -3,6 +3,10 @@ public class DemoJavaApp1 {
 
     public static void main(String[] args) {
         System.out.print("Hello Java Demojava app 1");
+        int a =20;
+        int b =30;
+        int c=a+b;
+        System.out.print("\na+b=" + c);
     }
     
 }
